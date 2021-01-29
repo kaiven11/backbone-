f70aa978599807ce3e9025000825137fe27b87f7
kaiven <1064187559@qq.com>
2021-01-29 11:29:26 +0800

    Update train.py
    手动实现网络，并加载预训练模型提取特征部分

--- vgg16复现/train.py
+++ vgg16复现/train.py
@@ -3,6 +3,7 @@
 from __future__ import print_function,division
 from vgg16 import vgg16
 import torch
+from tqdm import tqdm
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -11,7 +12,7 @@ from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt
 
-batch_size = 8
+batch_size = 64
 learning_rate = 0.0002
 epoch = 10
 
@@ -46,25 +47,54 @@ val_dataloader = torch.utils.data.DataLoader(val_datasets, batch_size=batch_size
  
 #==============================训练过程=========================================
 
-model = vgg16(num_classes=2)
+model = vgg16(num_classes=3)
 #检测网络是否可用
 if torch.cuda.is_available():
     torch.cuda.set_device(3)
     model.cuda() 
-params = [{'params': md.parameters()} for md in model.children()
-          if md in [model.classifier]]
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-loss_func = nn.CrossEntropyLoss()
+# params = [{'params': md.parameters()} for md in model.children()
+#           if md in [model.classifier]]
+
+
+
+#加载预训练模型
+
+net = torch.load("./model/vgg16-397923af.pth")
+
+
+pre_dict = net
+
+
+print(pre_dict.keys(),"predict")
+model_dict = model.state_dict()
+
+print(model_dict.keys(), "model_dict")
+
+
+
+update_dict = {k:v for k,v in pre_dict.items() if k in model_dict}
+
+print(update_dict.keys(),"update dict info")
+
+model_dict.update(update_dict)
+
+model.load_state_dict(model_dict)
+
+
+
+
+optimizer = optim.Adam(model.parameters(), lr=learning_rate) # 设置优化器
+loss_func = nn.CrossEntropyLoss() #设置损失函数
  
 Loss_list = []
 Accuracy_list = []
  
-for epoch in range(100):
+for epoch in range(5):
     print('epoch {}'.format(epoch + 1))
     # training-----------------------------
     train_loss = 0.
     train_acc = 0.
-    for batch_x, batch_y in train_dataloader:
+    for batch_x, batch_y in tqdm(train_dataloader):
         batch_x, batch_y = batch_x.cuda(), batch_y.cuda()
  
         out = model(batch_x)
@@ -86,7 +116,7 @@ for epoch in range(100):
     model.eval()
     eval_loss = 0.
     eval_acc = 0.
-    for batch_x, batch_y in val_dataloader:
+    for batch_x, batch_y in tqdm(val_dataloader):
         batch_x, batch_y = batch_x.cuda(), batch_y.cuda()
 
         out = model(batch_x)
@@ -102,20 +132,7 @@ for epoch in range(100):
     Accuracy_list.append(100 * eval_acc / (len(val_datasets)))
 #模型保存
 torch.save(model, './model/model.pth')
-#loss显示
-x1 = range(0, 100)
-x2 = range(0, 100)
-y1 = Accuracy_list
-y2 = Loss_list
-plt.subplot(2, 1, 1)
-plt.plot(x1, y1, 'o-')
-plt.title('Test accuracy vs. epoches')
-plt.ylabel('Test accuracy')
-plt.subplot(2, 1, 2)
-plt.plot(x2, y2, '.-')
-plt.xlabel('Test loss vs. epoches')
-plt.ylabel('Test loss')
-plt.savefig("./result.jpg")
+
 
 
 
